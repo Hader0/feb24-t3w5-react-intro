3e76b8ff0031a7e7a847500219ae09001cd0cfaa
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-09-10 20:28:44 +1000

    Render a list of food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+const food = [
+  "bananas",
+  "bacon",
+  "pizza",
+  "mango",
+  "nachos",
+  "KFC",
+  "sushi",
+  "enchilada"
+]
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+
+      <ul>
+        {
+          food.map(individualFoodItem => {
+            return <li>
+              {individualFoodItem}
+            </li>
+          })
+        }
+      </ul>
+
     </div>
+    
   );
 }
 
